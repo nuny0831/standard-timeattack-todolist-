92bff9b48b682d70fb81d169108f080a9526a2a7
nuny0831 <yrch92@gmail.com>
2023-11-07 11:40:05 +0900

    버튼기능구현

--- src/App.js
+++ src/App.js
@@ -1,23 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useState } from "react";
 
 function App() {
+  const [title, setTitle] = useState("");
+  const [content, setCotent] = useState("");
+  const [todos, SetTodos] = useState([]);
+
+  const titleChange = (event) => {
+    setTitle(event.target.value);
+  };
+
+  const contentChange = (event) => {
+    setCotent(event.target.value);
+  };
+
+  const addClick = () => {
+    SetTodos([
+      ...todos,
+      {
+        title,
+        content,
+        isDone: false,
+      },
+    ]);
+    setTitle("");
+    setCotent("");
+  };
+
+  const deleteClick = (index) => {
+    const newWork = todos.filter((work) => work !== todos[index]);
+    SetTodos(newWork);
+  };
+
+  const changeClick = (index) => {
+    todos[index].isDone = !todos[index].isDone;
+    SetTodos([...todos]);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <input value={title} onChange={titleChange} placeholder="제목"></input>
+        <input
+          value={content}
+          onChange={contentChange}
+          placeholder="내용"
+        ></input>
+        <button onClick={addClick}>생성</button>
+      </div>
+      <div>
+        할일목록
+        <div className="box">
+          {todos
+            .filter((todo) => todo.isDone === false)
+            .map((todo, index) => (
+              <div lassName="box">
+                <div>제목{todo.title}</div>
+                <div>내용{todo.content}</div>
+                <button onClick={() => deleteClick(index)}>삭제</button>
+                <button onClick={() => changeClick(index)}>완료</button>
+              </div>
+            ))}
+        </div>
+      </div>
+      <div>
+        완료목록
+        <div className="box">
+          {todos
+            .filter((todo) => todo.isDone === true)
+            .map((todo, index) => (
+              <div>
+                <div>제목{todo.title}</div>
+                <div>내용{todo.content}</div>
+                <button onClick={() => deleteClick(index)}>삭제</button>
+                <button onClick={() => changeClick(index)}>취소</button>
+              </div>
+            ))}
+        </div>
+      </div>
     </div>
   );
 }
